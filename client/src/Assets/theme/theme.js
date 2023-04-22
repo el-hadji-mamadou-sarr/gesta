e5e88f9a6b84aceaf1dcb4b63ba86f768d56b788
kaye romuald <kayeromy@gmail.com>
2023-04-23 01:18:33 +0200

    register form

---
+++ client/src/Assets/theme/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+import {blue} from "@mui/material/colors";
+
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#5AAC44'
+        }
+    }
+})
