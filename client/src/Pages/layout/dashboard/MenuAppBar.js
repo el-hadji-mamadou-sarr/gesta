f2337e62320294a0afea3a2c418595b1e702c35e
Marielle0709 <mariecisse@esp.sn>
2023-05-12 22:47:46 +0200

    register

--- client/src/Pages/layout/dashboard/MenuAppBar.js
+++ client/src/Pages/layout/dashboard/MenuAppBar.js
@@ -54,7 +54,7 @@ export default function MenuAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Photos
+            
           </Typography>
           {auth && (
             <div>
