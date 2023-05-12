3bafc5920804b580afcfa05456764adffad420f4
Marielle0709 <mariecisse@esp.sn>
2023-05-13 00:34:39 +0200

    dashboard

--- client/src/Pages/dashboard/dashboard.js
+++ client/src/Pages/dashboard/dashboard.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import * as React from "react";
 import MenuAppBar from "../../Component/navbar/dashboard/MenuAppBar";
 import { DrawerNavBar } from "../../Component/navbar/drawer/DrawerNavBar";
 import { CssBaseline, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import Box from '@mui/material/Box';
 
 export default function Dashboard() {
