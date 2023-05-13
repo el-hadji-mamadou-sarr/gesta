614b56b0ef4acf9ed2222966fa9a4e23bd3d5e67
gloatingorc <gloatingorc@gmail.com>
2023-05-14 01:29:24 +0200

    ajout de la documentation

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,9 +1,39 @@
-
 const express = require('express');
 const taskController = require('../controllers/taskController');
 const router = express.Router();
 const getId = require('../utils/getIdFromToken');
 
+
+
+/**
+ * @api {POST} /api/tasks/:project_id/:tab_id/:section_id/add
+ * @apiName AddTask
+ * @apiGroup Task
+ *
+ * 
+ * @RequestParam {String} project_id 
+ * @RequestParam {String} tab_id 
+ * @RequestParam {String} section_id
+ * 
+ *  
+ * @RequestBody {String} name 
+ * @RequestBody {String} description 
+ * @RequestBody {date} due_date 
+ * @RequestBody {OjectId[]} assigned_to[]  
+ * 
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 201 
+ *     {
+ *       message:"task created" ,
+ *     }
+ *
+ * @apiError InternalServer.
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 500 Internal server error
+ *     {
+ *       "message": "le serveur a rencontré un probléme"
+ *     }
+ */
 router.post('/:project_id/:tab_id/:section_id/add', async (req, res) => {
     const userId = getId(req);
 
