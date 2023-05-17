f9857d739f1f9378769e33bbc048661828f70715
Emmanuel DE PALMÈS <81565751+e2palmes@users.noreply.github.com>
2023-05-17 08:07:07 +0200

    profile edit form style

--- client/src/Pages/profile/Profile.js
+++ client/src/Pages/profile/Profile.js
@@ -8,6 +8,7 @@ import { theme } from "../../Assets/theme/theme";
 export default function Profile() {
 
   const [userInfos, setUserInfos] = useState({});
+  const [formProfileErrors, setFormProfileErrors] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/users/profile", { method: "GET", headers: {}, credentials: "include" })
@@ -17,41 +18,80 @@ export default function Profile() {
       })
   }, []);
 
+  function validate(data) {
+
+  }
+
+  function handleSubmitProfile(event) {
+    event.preventDefault();
+    validate(event.target);
+    console.log(event, event.target.email.value);
+  }
+
+  function handleProfileFieldChange(params) {
+
+  }
+
   return <>
     <MenuAppBar />
     <div className="w-1/2 mx-auto">
       <div className="flex justify-center mt-16 border-3 p-4" style={{ background: userInfos.banner_color }}>
-        <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-          <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+        <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+          <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
         </div>
       </div>
       <div>
         <div className="mt-8">
-          <form id="profileForm">
-            <div className="mt-4">
-              <label htmlFor="fullname">Nom complet: </label>
-              <input type="text" name="fullname" className="focus:border rounded-lg" value={userInfos.fullname} />
+          <form className="w-full max-w-lg mx-auto" onSubmit={handleSubmitProfile}>
+            <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full px-3">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                  Full name
+                </label>
+                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" name="fullname" value={userInfos.fullname} placeholder="john@doe.com" />
+                <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+              </div>
             </div>
-
-            <div className="mt-4">
-              <label htmlFor="fullname">Adresse electronique: </label>
-              <input type="text" name="fullname" className="focus:border rounded-lg" value={userInfos.fullname} />
+            <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full px-3">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-email">
+                  Email
+                </label>
+                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" name="email" value={userInfos.email} placeholder="john@doe.com" />
+                <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+              </div>
             </div>
-
-            <div>
-              <ThemeProvider theme={theme}>
-                <Button
-                  type="submit"
-                  color="registeBtnTheme"
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  <Box
-                    color="white">
-                    Enregistrer
-                  </Box>
-                </Button>
-              </ThemeProvider>
+            <div className="flex flex-wrap -mx-3 mb-2">
+              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-country">
+                  Pays
+                </label>
+                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque" />
+              </div>
+              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                  City
+                </label>
+                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque" />
+              </div>
+              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
+                  Zip
+                </label>
+                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210" />
+              </div>
+            </div>
+            <div className="md:flex md:items-center mt-8">
+              <div className="md:w-3/4 ">
+                <span className="font-bold py-2 px-4 rounded">
+                  <a href="#mdp" className="underline hover:text-blue-800">Reinitialiser mon mot de passe</a>
+                </span>
+              </div>
+              <div className="md:w-1/4 ">
+                <button className="shadow bg-blue-500 hover:bg-blue-800 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                  Enregistrer
+                </button>
+              </div>
             </div>
           </form>
         </div>
