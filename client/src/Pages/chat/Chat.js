55cfa1db6f8ca0ee81d8d3b0279c48483fe4474e
Emmanuel DE PALMÈS <81565751+e2palmes@users.noreply.github.com>
2023-05-25 11:22:42 +0200

    chat done

--- client/src/Pages/chat/Chat.js
+++ client/src/Pages/chat/Chat.js
@@ -90,7 +90,7 @@ function Chat() {
                     </div>
                     <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-1" />
                   </div>
-                </div> : <div className="chat-message">
+                </div> : <div className="chat-message" key={message._id}>
                   <div className="flex items-end justify-end">
                     <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
                       <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">{message.message}</span></div>
